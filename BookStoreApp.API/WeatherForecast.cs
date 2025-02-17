df96039c133851bc246b707a5a9dae17e25af095
Bilal Ahmed <info@bilal-ahmed-dev.com>
2025-02-17 17:08:51 +0300

    added logger and configured cors

--- BookStoreApp.API/WeatherForecast.cs
+++ BookStoreApp.API/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace BookStoreApp.API
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
